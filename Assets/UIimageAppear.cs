4f1ac98073f33448456c0234dee0008ed08e5f0b
droid1700 <60330760+droid1700@users.noreply.github.com>
2021-02-25 19:26:52 -0500

    Created New Image Trigger - Not Tested
    Created a trigger on an object that will hopefully display a test image. Not tested due to technical issues

---
+++ Assets/UIimageAppear.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIimageAppear : MonoBehaviour
+{
+    [SerializeField] private Image image_name;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			image_name.enabled = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			image_name.enabled = false;
+		}
+	}
+}
