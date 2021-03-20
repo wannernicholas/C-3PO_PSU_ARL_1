fa634b4eda6179d1a74fd3b78c79cee1c88d9a75
wannernicholas <71566742+wannernicholas@users.noreply.github.com>
2021-03-20 13:50:54 -0400

    Add files via upload

---
+++ Assets/Scripts/displayTextOnCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayTextOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    public string textToDisplay;
+    public GameObject objectToAccess;
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+	{
+		//image_name.gameObject.SetActive(true);
+		//ScriptName scriptToAccess = objectToAccess.GetComponent<PlayerManager>().SetPopUpText(textToDisplay);
+		objectToAccess.GetComponent<PlayerManager>().SetPopUpText(textToDisplay);
+// get the script on the object (make sure the script is a public class)      
+ 
+		//scriptToAccess.SetPopUpText(textToDisplay);
+	}
+	void OnTriggerExit(Collider other)
+	{
+		//image_name.gameObject.SetActive(false);
+		//ScriptName scriptToAccess = objectToAccess.GetComponent<PlayerManager>().SetPopUpText(textToDisplay);
+		objectToAccess.GetComponent<PlayerManager>().SetPopUpText("");
+// get the script on the object (make sure the script is a public class)      
+ 
+		//scriptToAccess.SetPopUpText("");
+	}
+}
