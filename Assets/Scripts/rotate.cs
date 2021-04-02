12a13b7242ba54c4e1e0ef645356ffad59daa93d
wannernicholas <71566742+wannernicholas@users.noreply.github.com>
2021-04-01 21:23:19 -0400

    Adding Rotate script for scenario 2

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string axisToRotate;
+    public float speed;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (axisToRotate.Equals("X")){
+        	transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+        else if(axisToRotate.Equals("Y")){
+        	transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        }
+        else{
+        	transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
