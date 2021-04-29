bf02a825ab6090ffe5d8f7ded73c2eccddc9f494
wannernicholas <71566742+wannernicholas@users.noreply.github.com>
2021-04-29 17:29:04 -0400

    anti cheese script

---
+++ Assets/Scripts/onColisTelepObj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onColisTelepObj : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    public Vector3 dest;
+    public GameObject objToTelepot;
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+	{
+		objToTelepot.transform.position  = dest;
+	}
+	
+}
